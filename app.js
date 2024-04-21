92f9fd31c5e7648dfc95c9d354641a778384e427
Islombek-2008 <playerislombek@gmail.com>
2024-04-22 01:15:19 +0500

    mobil-header

---
+++ app.js
@@ -0,0 +1,8 @@
+let menuBar = document.getElementById("menu")
+let openMenuBtn = document.getElementById("menu-btn")
+let body = document.getSelection("body")
+
+openMenuBtn.addEventListener("click", function () {
+    // body.style.display = "none"
+    menuBar.style.display = "block"
+  });
