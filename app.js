088db77640a323adc73b7d73d6dd95afda222090
Islombek-2008 <playerislombek@gmail.com>
2024-04-27 22:40:07 +0500

    mobile version

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let menuBar = document.getElementById("menu")
 let openMenuBtn = document.getElementById("menu-btn")
 let body = document.getSelection("body")
+let installPageMobile = document.getElementById("install-pageMobile")
 
 openMenuBtn.addEventListener("click", function () {
     // body.style.display = "none"
-    menuBar.style.display = "block"
+  menuBar.style.display = "block"
+  installPageMobile.style.display = "none"
   });
